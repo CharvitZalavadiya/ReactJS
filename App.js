5fac03a08231552bdaa9aa52372d6466c5dc94b8
Charvit Zalavadiya <120319997+CharvitZalavadiyaOfficial@users.noreply.github.com>
2023-10-22 15:46:19 +0530

    Added App Files

---
+++ App.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import "./App.css";
+// import Greet from "./components/5-greet"
+// import Welcome from "./components/6-welcome"
+// import Hellojsx from "./components/8-jsx"
+// import Props from "./components/9-props"
+// import Message from "./components/10-state"
+// import Counter from "./components/11-setstate"
+// import DeProps from "./components/12-destructuring_props_and_state"
+// import FunctionClick from "./components/13-event-handling-1"
+// import ClassClick from "./components/13-event-handling-2"
+// import EventBinding from "./components/14-event-binding";
+// import ParentComponent from "./components/15-method-as-props-1";
+// import UserGreeting from "./components/16-conditional-rendering";
+// import NameList from "./components/17-list-rendering-1";
+import StyleSheet from "./components/20-css-1";
+// import InlineCSS from "./components/20-css-2";
+// import './app-styles.css'     //20
+// import styles from './app-styles.module.css'      //20
+// import Form from './components/21-basics-of-form-handling'
+// import Mounting from "./components/23-component-mounting-lifecycle-method";
+// import Updating from "./components/24-component-updating-lifecycle";
+// import Fragment from "./components/25-fragments";
+// import Table from "./components/25-fragments-table";
+// import ParentComp from "./components/26-parent-comp";
+// import ParentComp from "./components/27-memo-parent-comp";
+// import Refs from "./components/28-refs";
+// import FocusInput from "./components/29-refs-with-class-comp-focus";
+// import FWParentInput from "./components/30-forwarding-ref-parent";
+// import Portals from "./components/31-portals";
+// import Hero from "./components/32-error-boundry-hero";
+// import ErrorBounding from "./components/32-error-boundry";
+// import ClickCounter from "./components/33-hoc-clickcounter";
+// import HoverCounter from "./components/33-hoc-hovercounter";
+// import ClickCounter from "./components/34-hoc-clickcounter";
+// import HoverCounter from "./components/34-hoc-hovercounter";
+// import CCounter from "./components/37-render-props-click-counter";
+// import HCounter from "./components/37-render-props-hover-counter";
+// import User from "./components/37-user";
+// import CompC from "./components/39-context-comp-c";
+// import { UserProvider } from "./components/39-user-provider";
+// import PostList from "./components/41-https-and-react";
+// import GetApi from "./components/42-http-get-request";
+// import PostForm from "./components/43-http-post-request";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello world !<br />
+        {/* <Greet /> */}
+        {/* <Welcome /> */}
+        {/* <Hellojsx /> */}
+        {/* <Props name="Chintan" age="14">
+        <h6>School</h6>
+      </Props>
+      <Props name="Charvit" age="19">
+        <button>Visit</button>
+      </Props>
+      
+      <Props name="Tilak" age="21" />
+      
+      <Props name="Mihir" age="23" />
+      
+      <Props name="Tanmay" age="23" /> */}
+        {/* <Message /> */}
+        {/* <Counter /> */}
+        {/* <DeProps name="CZ" age="19">
+        </DeProps> */}
+        {/* <FunctionClick />
+
+        <ClassClick /> */}
+        {/* <EventBinding /> */}
+        {/* <ParentComponent /> */}
+        {/* <UserGreeting /> */}
+        {/* <NameList /> */}
+        <StyleSheet primary={true}/>
+        {/* <InlineCSS /> */}
+        {/* <h1 className="no">NO</h1> */}
+        {/* <h1 className={styles.yes}>YES</h1> */}
+        {/* <Form /> */}
+        {/* <Mounting /> */}
+        {/* <Updating /> */}
+        {/* <Fragment />
+        <Table /> */}
+        {/* <ParentComp /> */}
+        {/* <Refs /> */}
+        {/* <FocusInput /> */}
+        {/* <FWParentInput /> */}
+        {/* <Portals /> */}
+        {/* <ErrorBounding>
+          <Hero heroName="Batman" />
+        </ErrorBounding>
+        <ErrorBounding>
+          <Hero heroName="Superman" />
+        </ErrorBounding>
+        <ErrorBounding>
+          <Hero heroName="Joker" />
+        </ErrorBounding> */}
+        {/* <ClickCounter name='CZ '/>
+        <HoverCounter name='ZC '/> */}
+        {/* <CCounter />
+        <HCounter />
+        <User name={(isLoggedIn) => isLoggedIn ? "CZ" : "New"}/> */}
+        {/* <UserProvider value="CZ">
+          <CompC />
+        </UserProvider> */}
+
+        {/* <PostList /> */}
+        {/* <GetApi /> */}
+        {/* <PostForm /> */}
+      </div>
+    );
+  }
+}
+
+console.log("hello cz");
+
+export default App;
